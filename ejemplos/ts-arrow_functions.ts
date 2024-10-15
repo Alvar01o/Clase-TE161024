57f5f812763bd8057c2343eec42fa3141974d333
Alvaro <alvar01omer@gmail.com>
2024-10-15 00:05:51 -0300

    limpieza

--- ejemplos/ts-arrow_functions.ts
+++
@@ -1,5 +0,0 @@
-const getFullName = (Nombre:string, Apellido:string) : string => {
-    return `${Nombre} ${Apellido}`;
-}
-let nombreCompleto = getFullName('Ricardo', 'Hernandez');
-console.log(nombreCompleto);
