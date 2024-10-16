144073da36e38971ae2210713163a52c4ef64f23
Alvaro <alvar01omer@gmail.com>
2024-10-16 01:28:33 -0300

    ppt

---
+++ ejemplos/promesas/ejercicio.js
@@ -0,0 +1,13 @@
+/**
+ * Usar promesas para realizar un proceso asíncrono con (fetch)
+ * usar esta api para obtener la url de una imagen:
+ * https://dog.ceo/api/breeds/image/random
+ */
+// Tu código aca:
+function obtenerImagen() {
+    return new Promise((resolve, reject) => {
+        // implementa tu código
+    });
+}
+
+obtenerImagen().then(data => console.log(data));
